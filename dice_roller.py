4095eff7d34f6dbcef5c4916f764d730dddb4667
Morrice R. Smith <smith_mr@cobra.cs.mercer.edu>
2020-07-27 20:03:13 -0400

    Last step maybe?

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,14 @@
+import random
 def main():
-  print('You rolled a die')
+  dice_sum = 0
+  dice_rolls = 3
+  for i in range (0,dice_rolls):
+    roll = random.randint(1,6)
+    print(f'You rolled a {roll}')
+    dice_sum += roll
+  print(f'Your total roll is {dice_sum}')
+
+    	
 
 if __name__== "__main__":
   main()
