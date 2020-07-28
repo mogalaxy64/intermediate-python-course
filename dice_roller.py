d4e27aa541dbc819fdbbb84ca5f6b3bd5dfe138b
Morrice R. Smith <smith_mr@cobra.cs.mercer.edu>
2020-07-27 20:15:36 -0400

    finished dice roller in python baby

--- dice_roller.py
+++ dice_roller.py
@@ -1,10 +1,18 @@
 import random
 def main():
   dice_sum = 0
-  dice_rolls = 3
+  dice_sides = int(input('How many sides are on the dice?'))
+  dice_rolls = int(input('How many dice do you want to roll?'))
   for i in range (0,dice_rolls):
-    roll = random.randint(1,6)
-    print(f'You rolled a {roll}')
+    
+    
+    roll = random.randint(1,dice_sides)
+    if roll == 1:
+      print(f'Critical Fail! You rolled a {roll}')
+    elif roll == dice_sides: 
+      print(f'Damn son! You rolled a {roll}')
+    else:
+      print(f'You rolled a {roll}')
     dice_sum += roll
   print(f'Your total roll is {dice_sum}')
 
